19b663564cf30b64bed1793f2da3d38103e328ed
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-03-05 13:57:31 +0100

    hämta alla spelare klar

--- src/main/java/LeffesVHS/service/VhsPlayerService.java
+++ src/main/java/LeffesVHS/service/VhsPlayerService.java
@@ -1,5 +1,22 @@
 package LeffesVHS.service;
 
+import java.util.List;
+
+import LeffesVHS.model.VhsPlayer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@Transactional(Transactional.TxType.SUPPORTS) 
+@ApplicationScoped
 public class VhsPlayerService {
 
+    @Inject
+    EntityManager entityManager;
+
+     public List<VhsPlayer> findAll(){
+        List<VhsPlayer> players = entityManager.createQuery("SELECT v FROM VhsPlayer v", VhsPlayer.class).getResultList();
+        return players;
+    }
 }
